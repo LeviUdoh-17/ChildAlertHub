67c0decf88f3522a2bc7f3e0cd7f87b2f0152867
LeviUdoh-17 <leviudoh17@gmail.com>
2024-12-29 22:43:10 +0100

    Refactor navbar styles and clean up home page links

--- static/app.js
+++ static/app.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener("scroll", function() {
         const navbar = document.querySelector(".navbar");
         if (window.scrollY > 50) {
-            document.body.classList.add("scrolled"); 
+            document.body.classList.add("scrolled");
         } else {
             document.body.classList.remove("scrolled");
         }
